a49e376592a25d95e494b396a726efb989554377
Sidney Calloway Jr <sid@lovefateventures.com>
2019-10-09 13:39:26 -0700

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,22 @@
+// October 8th 2019 //
+// First Day of Writing Programs//
+// Project #00001 | 'Hello World' //
+// Language: C++ //
+// Blessed Sid Build //
+
+
+#include <iostream>
+
+using namespace std; 
+
+int main()
+{
+
+//Print out to user //
+
+cout << "Hello World!" << endl ;
+return 0;
+}
+
+//---Respectfully,
+//---Sid Calloway//
